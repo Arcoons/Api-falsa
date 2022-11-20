f720d6fa6163cd079fdde696da93bdd83593a334
Christian <christianzaor1090@gmail.com>
2022-11-20 16:56:39 -0500

    api

--- src/server.js
+++ src/server.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const cors = require("cors");
 const morgan = require('morgan');
 const conexionBD = require('./conexion');
 const rutasAlbum = require('./routes/album.routes');
 const rutasCancion=require('./routes/cancion.routes');
 const rutasGenero=require('./routes/genero.routes');
 const app = express()
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 4000;
 conexionBD();
 
 
 app.set("name","disquera");
-app.set("port",process.env.port || 3500);
+app.set("port",process.env.port || 4000);
 
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/api/album",rutasAlbum);
-app.use("/api/cancion",rutasCancion);
-app.use("/api/genero",rutasGenero);
+app.use("/album",rutasAlbum);
+app.use("/cancion",rutasCancion);
+app.use("/genero",rutasGenero);
 
 
 module.exports=app;
