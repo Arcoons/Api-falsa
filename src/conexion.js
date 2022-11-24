08ee04f732b333632ed0a7e63fe7a6fd3ed9e535
Spike <104435798+Arcoons@users.noreply.github.com>
2022-11-24 11:36:33 -0500

    conexion

--- src/conexion.js
+++ src/conexion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const conexionBD=async()=>{
     
     try{
-        const DB=await mongoose.connect('mongodb+srv://spike:6SNrR3eXVJbcLOmm@cluster0.kuapygk.mongodb.net/?retryWrites=true&w=majority');
+        const DB=await mongoose.connect('mongodb+srv://spike:6SNrR3eXVJbcLOmm@cluster0.kuapygk.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
         console.log("Conexión satisfactoria",DB.connection.name);
     }
     catch(  error){
